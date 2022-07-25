d8e99a332e9a3c7f48515dbdf7374f86725dee07
Viktor Triber <vtriber@ya.ru>
2022-07-25 23:28:53 +0300

    Добавил прогресс-бар

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import requests
 from pprint import pprint
 from vktoken import vktoken
 
-#
 class VkLoading():
     def __init__(self, token, owner_id, folder):
         self.token = token
@@ -43,7 +42,6 @@ class VkLoading():
             photo_json['size'] = size
             all_photo_json.append(photo_json)
 
-
         os.mkdir(self.folder)
         for filename, url in photo_copy.items():
             path_file = self.folder + '/' + filename
@@ -78,15 +76,28 @@ class YandexDisk:
         response = requests.put(href, data=open(f'{os.getcwd()}/{disk_file_path}/{file}', 'rb'))
 
 
+def API_VK():
+    owner_id = input('Введите ID аккаунта Вконтакте: ')
+    folder = input('Введите имя папки куда будут загружены фотографии: ')
+    vk = VkLoading(token=vktoken, owner_id=owner_id, folder=folder)
+    vk.loading_photo()
 
-owner_id = input('Введите ID аккаунта Вконтакте: ')
-folder = input('Введите имя папки куда будут загружены фотографии: ')
-vk = VkLoading(token=vktoken, owner_id=owner_id, folder=folder)
-vk.loading_photo()
+    token = input('Введите токен для доступа к Яндекс Диску: ')
+    ya = YandexDisk(token=token)
+    ya.creat_folder(disk_file_path=folder)
 
-token = input('Введите токен для доступа к Яндекс Диску: ')
-ya = YandexDisk(token=token)
-ya.creat_folder(disk_file_path=folder)
-file_list = os.listdir(folder)
-for file in file_list:
-    ya.upload_file_to_disk(disk_file_path=folder)
+    file_list = os.listdir(folder)
+    hundred_percent = len(file_list)
+    file_percent = 100 // hundred_percent
+    symbol_percent = '#'
+    percent = symbol_percent * file_percent
+    step = 0
+    for file in file_list:
+        ya.upload_file_to_disk(disk_file_path=folder)
+        step += 1
+        print('\n' * 100)
+        print('Ход загрузки:')
+        print(f'{percent * step} Загружено {file_percent * step} %')
+
+if __name__ == '__main__':
+    API_VK()
